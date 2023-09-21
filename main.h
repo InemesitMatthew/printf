b4043c1885dea1ae1e12e2bf32c99da95d0349c9
akanSWE <a.akanSWE@gamil.com>
2023-09-21 14:37:07 +0100

    Update header file, with more function prototypes, and macros for the programs

--- main.h
+++ main.h
@@ -1,7 +1,73 @@
 #ifndef PRINTF
 #define PRINTF
 
+/*programs macros*/
+#define NULL_BYTE 6
 #define UINT_SIZE 32
+#define BYTE 1
+
+/*system headers*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
+
+/**
+ * handle_char - handles format specifier c "%c"
+ * to print character
+ *
+ * @character: character from the argument
+ * @countByte: keeps count of the number of character printed in bytes
+ *
+ * Return: void (Nothing)
+ */
+void handle_char(char character, int *countByte);
+
+/**
+ * handle_string - handles format specifier s "%s"
+ * to print stirng
+ *
+ * @str_buffer: string from the argument
+ * @countByte: keeps count of the number of characters printed in bytes
+ *
+ * Return: void (Nothing)
+ */
+void handle_string(char *str_buffer, int *countByte);
+
+/**
+ * handle_percent - handles the format specifier % "%%"
+ * to print percent sign
+ *
+ * @character: character from the argument
+ * @countByte: keeps count of the number of character printed in bytes
+ *
+ * Return: void (Nothing)
+ */
+void handle_percent(char character, int *countByte);
+
+/**
+ * handle_integers - handles the format specifier d and i "%d", "%i"
+ * to print integers
+ *
+ * @number: number from the argument
+ * @num_buffer: store the number as a string
+ * @countByte: keeps count of the number printed in bytes
+ *
+ * Return: void (Nothing)
+ */
+void handle_integers(int number, char *num_buffer, int *countByte);
+
+/**
+ * handle_reverse_string - handles the format specifier r "%r"
+ * to print string reverse
+ *
+ * @str_buffer: string from argument
+ * @countByte: keeps count of the number of characters printed out in bytes
+ *
+ * Return: void (Nothing)
+ */
+void handle_reverse_string(char *str_buffer, int *countByte);
 
 #endif /* PRINTF */
