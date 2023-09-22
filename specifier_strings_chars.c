4d5dff09c4f5f2f8ff84c4800f9cadd1131b63ee
akanSWE <a.akanSWE@gamil.com>
2023-09-22 18:22:47 +0100

    Add rot_13 function implementation and also corrected some descriptions

--- specifier_strings_chars.c
+++ specifier_strings_chars.c
@@ -6,7 +6,7 @@
  *
  * @character: character from the argument
  *
- * Return: void (Nothing)
+ * Return: BYTE which is 1 for a single character
  */
 int handle_char(char character)
 {
@@ -21,7 +21,8 @@ int handle_char(char character)
  *
  * @str_buffer: string from the argument
  *
- * Return: void (Nothing)
+ * Return: The length of the bytes printed otherwise
+ * return NULL_BYTE which is 6 if null is passed as the argument
  */
 int handle_string(char *str_buffer)
 {
@@ -48,7 +49,7 @@ int handle_string(char *str_buffer)
  *
  * @character: character from the argument
  *
- * Return: void (Nothing)
+ * Return: BYTE which is 1 for character %
  */
 int handle_percent(char character)
 {
@@ -63,7 +64,8 @@ int handle_percent(char character)
  *
  * @str_buffer: string from argument
  *
- * Return: void (Nothing)
+ * Return: The length of the bytes printed otherwise
+ * return NULL_BYTE which is 6 if null is passed as the argument
  */
 int handle_reverse_string(char *str_buffer)
 {
@@ -94,3 +96,46 @@ int handle_reverse_string(char *str_buffer)
 		return (NULL_BYTE);
 	}
 }
+
+/**
+ * handle_rot13 - handles the format specifier R "%R"
+ * to print string in rot 13 representation
+ *
+ * @str: string from the argument
+ *
+ * Return: The length of the printed rot 13 string
+ */
+int handle_rot13(char *str)
+{
+	int countByte;
+	char rot_13; /* Used to hold the current conversion to rot 13 */
+
+	/* Convert to rot13 */
+	countByte = 0;
+	while (*str != '\0')
+	{
+
+	if (*str >= 'A' && *str <= 'Z')
+	{
+	/* Convert to upper case and output */
+		rot_13 = (((*str - 'A') + 13) % 26) + 'A';
+	}
+	else if (*str >= 'a' && *str <= 'z')
+	{
+	/* Convert to lower case and output */
+		rot_13 = (((*str - 'a') + 13) % 26) + 'a';
+	}
+	else
+	{
+		rot_13 = *str; /* Not an alphabet so just output */
+	}
+
+	/* Control get here after each conversion so print the current character */
+	write(1, &rot_13, BYTE);
+	countByte += BYTE;
+
+	str++;
+	}
+	return (countByte); /* Total number of character outputed */
+}
+
