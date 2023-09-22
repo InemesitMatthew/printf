615e031655c3aa52512c1eb34d545f9c89d3b1cc
akanSWE <a.akanSWE@gamil.com>
2023-09-22 18:20:43 +0100

    Add more function prototypes for the format specifiers, and corrected some descriptions

--- main.h
+++ main.h
@@ -14,6 +14,14 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * _printf - format and print data
+ *
+ * @format: formated string
+ * ...: variable number of arguments
+ *
+ * Return: The total count or length of data printed to stdout
+ */
 int _printf(const char *format, ...);
 
 /**
@@ -22,7 +30,7 @@ int _printf(const char *format, ...);
  *
  * @character: character from the argument
  *
- * Return: void (Nothing)
+ * Return: BYTE which is 1 for a single character
  */
 int handle_char(char character);
 
@@ -32,7 +40,8 @@ int handle_char(char character);
  *
  * @str_buffer: string from the argument
  *
- * Return: void (Nothing)
+ * Return: The length of the bytes printed otherwise
+ * return NULL_BYTE which is 6 if null is passed as the argument
  */
 int handle_string(char *str_buffer);
 
@@ -42,7 +51,7 @@ int handle_string(char *str_buffer);
  *
  * @character: character from the argument
  *
- * Return: void (Nothing)
+ * Return: BYTE which is 1 for character %
  */
 int handle_percent(char character);
 
@@ -52,7 +61,8 @@ int handle_percent(char character);
  *
  * @str_buffer: string from argument
  *
- * Return: void (Nothing)
+ * Return: The length of the bytes printed otherwise
+ * return NULL_BYTE which is 6 if null is passed as the argument
  */
 int handle_reverse_string(char *str_buffer);
 
@@ -62,17 +72,19 @@ int handle_reverse_string(char *str_buffer);
  *
  * @number: number from the argument
  *
- * Return: void (Nothing)
+ * Return: The length of the number, which consist of each digit
  */
 int handle_integers(int number);
 
 /**
- * handle_binary - handle the format specifier b "%b"
+ * handle_binary - handles the format specifier b "%b"
  *
  * @number: number from the argument
- * printed int bytes
  *
- * Return: void (Nothing);
+ * Return:
+ * - The length of the Printed binary
+ * - 0 if the number is not unsigned int
+ * - 1 if the number is equal to zero
  */
 int handle_binary(int number);
 
@@ -82,8 +94,59 @@ int handle_binary(int number);
  *
  * @number: unsigned integer from argument
  *
- * Return: void (Nothing)
+ * Return: The length of the printed unsigned int
  */
 int handle_unsigned_integers(unsigned int number);
+
+/**
+ * handle_octal - handles the format specifier o "%o"
+ * to print octal representation of numbers
+ *
+ * @number: number from the argument
+ *
+ * Return: The length of the printed octal number
+ */
+int handle_octal(int number);
+
+/**
+ * handle_lowerCase_hexadecimal - handles the format specifier x "%x"
+ * to print hexadecimal representation of numbers in lower-case
+ *
+ * @number: number from the argument
+ *
+ * Return: The length of the printed hexadecimal number
+ */
+int handle_lowerCase_hexadecimal(int number);
+
+/**
+ * handle_upperCase_hexadecimal - handles the format specifier X "%X"
+ * to print hexadecimal representation of numbers in upper-case
+ *
+ * @number: number from the argument
+ *
+ * Return: The length of the printed hexadecimal number
+ */
+int handle_upperCase_hexadecimal(int number);
+
+
+/**
+ * handle_pointer - handles the format specifier p "%p"
+ * to print memory addresses
+ *
+ * @ptr: pointer that stores address
+ *
+ * Return: The length of the printed address
+ */
+int handle_pointer(void *ptr);
+
+/**
+ * handle_rot13 - handles the format specifier R "%R"
+ * to print string in rot 13 representation
+ *
+ * @str: string from the argument
+ *
+ * Return: The length of the printed rot 13 string
+ */
+int handle_rot13(char *str);
 
 #endif /* PRINTF */
