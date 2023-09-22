318b8008ace58d966a2113fe6e28c3de6bb620a1
akanSWE <a.akanSWE@gamil.com>
2023-09-22 13:50:05 +0100

    Update header file
    Changed the return type of each function prototype to int, and also their
    descriptions, removed countByte parameter, because number of byte should
    be updated from the _printf function

--- main.h
+++ main.h
@@ -7,7 +7,9 @@
 #define BYTE 1
 
 /*system headers*/
+#include <limits.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -19,76 +21,69 @@ int _printf(const char *format, ...);
  * to print character
  *
  * @character: character from the argument
- * @countByte: keeps count of the number of character printed in bytes
  *
  * Return: void (Nothing)
  */
-void handle_char(char character, int *countByte);
+int handle_char(char character);
 
 /**
  * handle_string - handles format specifier s "%s"
  * to print stirng
  *
  * @str_buffer: string from the argument
- * @countByte: keeps count of the number of characters printed in bytes
  *
  * Return: void (Nothing)
  */
-void handle_string(char *str_buffer, int *countByte);
+int handle_string(char *str_buffer);
 
 /**
  * handle_percent - handles the format specifier % "%%"
  * to print percent sign
  *
  * @character: character from the argument
- * @countByte: keeps count of the number of character printed in bytes
  *
  * Return: void (Nothing)
  */
-void handle_percent(char character, int *countByte);
+int handle_percent(char character);
 
 /**
  * handle_reverse_string - handles the format specifier r "%r"
  * to print string reverse
  *
  * @str_buffer: string from argument
- * @countByte: keeps count of the number of characters printed out in bytes
  *
  * Return: void (Nothing)
  */
-void handle_reverse_string(char *str_buffer, int *countByte);
+int handle_reverse_string(char *str_buffer);
 
 /**
  * handle_integers - handles the format specifier d and i "%d", "%i"
  * to print integers
  *
  * @number: number from the argument
- * @countByte: keeps count of the numbers of each digit, printed in bytes
  *
  * Return: void (Nothing)
  */
-void handle_integers(int number, int *countByte);
+int handle_integers(int number);
 
 /**
  * handle_binary - handle the format specifier b "%b"
  *
  * @number: number from the argument
- * @countByte: keeps count of the binary number of each digit
  * printed int bytes
  *
  * Return: void (Nothing);
  */
-void handle_binary(int number, int *countByte);
+int handle_binary(int number);
 
 /**
  * handle_unsigned_integers - handles the format specifier u "%u"
  * to print unsigned integers
  *
  * @number: unsigned integer from argument
- * @countByte: keeps count of the number of each digit, printed in bytes
  *
  * Return: void (Nothing)
  */
-void handle_unsigned_integers(unsigned int number, int *countByte);
+int handle_unsigned_integers(unsigned int number);
 
 #endif /* PRINTF */
