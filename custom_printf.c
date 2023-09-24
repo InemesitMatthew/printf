894e497ece6f21b53e25f0cd3d17bed4ddd81304
akanSWE <a.akanSWE@gamil.com>
2023-09-24 01:17:53 +0100

    Defined the structure for printing format string
    Defined struct 'specifierHandlers' that maps specifier to each corresponding function
    and implemented function handle_format_string, to handle the format string

--- custom_printf.c
+++ custom_printf.c
@@ -1,137 +1,97 @@
-#include <stdio.h>
 #include "main.h"
+
+struct specifierHandlers specifierHandlers[]
+	= {{'b', handle_binary},
+	   {'c', handle_char},
+	   {'d', handle_integers},
+	   {'i', handle_integers},
+	   {'u', handle_unsigned_integers},
+	   {'o', handle_octal},
+	   {'x', handle_lowerCase_hexadecimal},
+	   {'X', handle_upperCase_hexadecimal},
+	   {'p', handle_pointer},
+	   {'s', handle_string},
+	   {'S', handle_upperCase_S},
+	   {'%', handle_percent},
+	   {'r', handle_reverse_string},
+	   {'R', handle_rot13}};
 
 /**
  * _printf - Custom printf function
+ *
  * @format: The format string
  * @...: Variable number of arguments
  *
- * Return: The number of characters printed (excluding the null byte).
+ * Return: The number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int count;
 
-	int count, num_int;	/* num_int is number as an integer */
-	unsigned int num_un; /* num_un is a number as an unsigned integer */
-	void *ptr;
-	char *str;
-
-	char buffer[1024]; /* Local buffer for write */
-	char character;
-
-	char *str_buffer = buffer; /* Pointer to buffer for string handling */
+	count = 0;
 
 	va_start(args, format); /* Initialize the va_list */
 
-	if (format == NULL)
-		return (-1); /* Return error, it a null*/
+	count += handle_format_string(format, args);
 
-	count = 0;
+	va_end(args); /* Clean up the va_list */
+	return (count);
+}
+
+/**
+ * handle_format_string - Processes a format string,
+ * prints it out, replacing format specifiers with
+ * values from the argument list.
+ *
+ * @format: The format string, may contain zero or more
+ * format specifiers.
+ * @args: A va_list containing arguments to replace the
+ * format specifiers.
+ *
+ * Return: The number of characters printed, excluding
+ * the null byte.
+ */
+int handle_format_string(const char *format, va_list args)
+{
+	int count, i, size;
+	bool match_found;
+
+	count = 0, match_found = false;
+	size = sizeof(specifierHandlers) / sizeof(specifierHandlers[0]);
+
+	if (format == NULL)
+		return (-1); /* Error */
+
 	while (*format)
 	{
-		if (*format == '\0') /* Break immediately you meet a null */
-			break;
-
 		if (*format != '%')
 		{
 			write(1, format, BYTE);
-			count += BYTE;
+			count++;
 		}
 		else
 		{
-			 /* Move past '%' check the next character to determine how to format */
-			format++;
-			switch (*format) /* Handle format specifier */
+			format++; /* Move past '%' to the next character */
+			for (i = 0; i < size; i++)
 			{
-			case 'b':
-				/* Handle binary conversion */
-				num_un = va_arg(args, unsigned int);
-				count += handle_binary(num_un);
-				break;
-
-			case 'c':
-				/* Handle character conversion */
-				character = va_arg(args, int);
-				count += handle_char(character);
-				break;
-
-			case 'd':
-			case 'i':
-				/* Handle decimal and integer conversion */
-				num_int = va_arg(args, int);
-				count += handle_integers(num_int);
-				break;
-
-			case 'u':
-				/* Handle unsigned integer conversion */
-				num_un = va_arg(args, unsigned int);
-				count += handle_unsigned_integers(num_un);
-				break;
-
-			case 'o':
-				/* Handle octal conversion */
-				num_un = va_arg(args, unsigned int);
-				count += handle_octal(num_un);
-				break;
-
-			case 'x':
-				/* Handle lowercase hexadecimal conversion */
-				num_un = va_arg(args, unsigned int);
-				count += handle_lowerCase_hexadecimal(num_un);
-				break;
-
-			case 'X':
-				/* Handle uppercase hexadecimal conversion */
-				num_un = va_arg(args, unsigned int);
-				count += handle_upperCase_hexadecimal(num_un);
-				break;
-
-			case 'p':
-				/* Handle pointer conversion */
-				ptr = va_arg(args, void *);
-				count += handle_pointer(ptr);
-				break;
-
-			case 's':
-				/* Handle string conversion */
-				str_buffer = va_arg(args, char *);
-				count += handle_string(str_buffer);
-				break;
-
-      case 'S':
-          str_buffer = va_arg(args, char *);
-          count += handle_upperCase_S(str_buffer);
-          break;
-
-			case '%':
-
-				count += handle_percent(*format);
-				break;
-
-			case 'r':
-				/* Handle reversed string conversion */
-				str = va_arg(args, char *);
-				count += handle_reverse_string(str);
-				break;
-
-			case 'R':
-				/* Handle rot13'ed string conversion */
-				str = va_arg(args, char *);
-				count += handle_rot13(str);
-				break;
-
-			default:
-				/* Handle unsupported format specifier */
-				write(1, "%", 1);
-				write(1, format, 1);
-				count += 2;
-				break;
+				/* Check if the format matches any specifier from the array */
+				if (specifierHandlers[i].specifier == *format)
+				{
+					count += specifierHandlers[i].handler(args);
+					match_found = true;
+					break; /* Move to the next format */
+				}
+			}
+			/* No match after loop, so execute */
+			if (!match_found)
+			{
+				write(1, "%", BYTE);
+				write(1, format, BYTE);
+				count += (BYTE + BYTE);
 			}
 		}
-		format++; /* Move to the next character in the format string */
+		format++;
 	}
-	va_end(args); /* Clean up the va_list */
-
 	return (count);
 }
